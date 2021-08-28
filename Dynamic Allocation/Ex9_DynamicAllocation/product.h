10db0a5f2f0f8f59cd4d15efd4815942d47bfac2
Matheus Miland <matheus.miland@gmail.com>
2021-08-28 02:59:38 -0300

    Tuning the Ex9 in the dynamicallocation projects

--- Dynamic Allocation/Ex9_DynamicAllocation/product.h
+++ Dynamic Allocation/Ex9_DynamicAllocation/product.h
@@ -1,11 +1,6 @@
-typedef struct st_product
-{
-    int code, qty;
-    char name[50];
-    float price;
-} product;
+typedef struct st_product product; //Product information struct.
 
-product* r_products(int N); //The number of products is passed to this function, then she creates the vector of products.
+product* r_products(int N); //The number of products is passed to this function, which then creates the list of products.
 int hValue(int N, product* p); //Checks the products for the most valuable item.
 int hQty(int N, product* p); //Checks the products for the item in the largest quantity.
-void print_products(int N, int mValueIndex, int lQtyIndex, product* p); //This function prints all the products.
+void print_products(int N, int mValueIndex, int lQtyIndex, product* p); //This function prints all the products, the most valuable product, and also the product in the largest quantity.
